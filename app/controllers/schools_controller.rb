dbbcf9e06dce1af328b01a3b4eb53ab0ce3fe18a
haemalatha <haemalatha.r@in.reportbee.com>
2014-10-09 19:57:27 +0530

    listing schools created withoy using scaffold

---
+++ app/controllers/schools_controller.rb
@@ -0,0 +1,24 @@
+class SchoolsController < ApplicationController
+  def index
+    @schools = School.all
+  end
+  def new
+    @school=School.new
+    end
+
+  def create
+     @school = School.new(school_params)
+    if  @school.save
+    redirect_to @school
+    else
+      render 'new'
+    end
+  end
+  def show
+    @school = School.find(params[:id])
+  end
+  private
+  def school_params
+    params.require(:school).permit(:name, :address)
+  end
+end
